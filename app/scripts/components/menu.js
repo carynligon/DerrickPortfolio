b1b199c1bafe9c822887342c28d0bb492bae5836
Caryn Ligon <carynligon@gmail.com>
2016-11-09 10:54:51 -0600

    sliding menu working properly

--- app/scripts/components/menu.js
+++ app/scripts/components/menu.js
@@ -2,18 +2,33 @@ import React from 'react';
 
 class Menu extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state= {showMenu: false, openClass: false};
+  }
+  hamburgerClick(e) {
+    this.setState({openClass: !this.state.openClass});
   }
   render() {
-    return (`
-      <aside>
-        <ul className="menu-list">
-          <li>link 1</li>
-          <li>link 2</li>
-          <li>link 3</li>
+    let openClassName;
+    if (this.state.openClass) {
+      openClassName = 'open'
+    }
+    return (
+      <aside className={openClassName}>
+        <div onClick={this.hamburgerClick.bind(this)} id="nav-icon4" className={openClassName}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className="menu=list">
+          <li>Link</li>
+          <li>Link</li>
+          <li>Link</li>
+          <li>Link</li>
+          <li>Link</li>
         </ul>
       </aside>
-      `);
+      );
   }
 }
 
--- app/scripts/pages/app.js
+++ app/scripts/pages/app.js
@@ -5,22 +5,12 @@ import Menu from '../components/menu';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state= {showMenu: false, openClass: false};
-  }
-  hamburgerClick(e) {
-    this.setState({openClass: !this.state.openClass});
   }
   render() {
-    let openClassName;
-    if (this.state.openClass) {
-      openClassName = 'open'
-    }
     return (
-      <div onClick={this.hamburgerClick.bind(this)} id="nav-icon4" className={openClassName}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+      <main>
+        <Menu/>
+      </main>
       );
   }
 }
